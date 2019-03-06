1d7b61e07c2c5c987894948b6781bd41afd70401
Idar Borlaug <idar.borlaug@gmail.com>
2019-03-06 09:36:06 +0100

    Endret til å bruke kryptering biblioteket vårt

--- src/main/java/no/ks/svarut/dekrypter/Dekrypter.java
+++ src/main/java/no/ks/svarut/dekrypter/Dekrypter.java
@@ -1,23 +1,57 @@
 package no.ks.svarut.dekrypter;
 
 
+import no.ks.kryptering.CMSKrypteringImpl;
 import org.apache.commons.io.IOUtils;
+import org.bouncycastle.asn1.pkcs.PrivateKeyInfo;
+import org.bouncycastle.openssl.PEMDecryptorProvider;
+import org.bouncycastle.openssl.PEMEncryptedKeyPair;
+import org.bouncycastle.openssl.PEMKeyPair;
+import org.bouncycastle.openssl.PEMParser;
+import org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter;
+import org.bouncycastle.openssl.jcajce.JcePEMDecryptorProviderBuilder;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.InputStream;
+import java.io.*;
+import java.security.KeyPair;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.spec.InvalidKeySpecException;
 
 public class Dekrypter {
-    private final CMSDataKryptering cmsDataKryptering;
+    private final CMSKrypteringImpl cmsDataKryptering;
     private Config config;
+    private PrivateKey privatekey;
 
     public Dekrypter(Config config) {
         this.config = config;
         try {
-            cmsDataKryptering = new CMSDataKryptering(config.getPrivateKey(), config.getPrivateKeyPassword());
+            privatekey = getPrivateKey(config.getPrivateKey(), config.getPrivateKeyPassword());
+            cmsDataKryptering = new CMSKrypteringImpl();
         } catch (Exception e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    private PrivateKey getPrivateKey(File privateKeyFile, String password) throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
+        System.out.println();
+        System.out.println("reading key " + privateKeyFile.getAbsolutePath());
+        if(password == null) password = "";
+        PEMParser pemParser = new PEMParser(new FileReader(privateKeyFile));
+        Object object = pemParser.readObject();
+        PEMDecryptorProvider decProv = new JcePEMDecryptorProviderBuilder().build(password.toCharArray());
+        JcaPEMKeyConverter converter = new JcaPEMKeyConverter().setProvider("BC");
+        KeyPair kp;
+        if (object instanceof PEMEncryptedKeyPair) {
+            System.out.println("Encrypted key - we will use provided password");
+            kp = converter.getKeyPair(((PEMEncryptedKeyPair) object).decryptKeyPair(decProv));
+            return kp.getPrivate();
+        }else if (object instanceof PrivateKeyInfo){
+            PrivateKeyInfo key = (PrivateKeyInfo) object;
+            return converter.getPrivateKey(key);
+        } else {
+            System.out.println("Unencrypted key - no password needed");
+            kp = converter.getKeyPair((PEMKeyPair) object);
+            return kp.getPrivate();
         }
     }
 
@@ -41,7 +75,7 @@ public class Dekrypter {
     private void dekrypt(File source, File target) {
         try(FileInputStream is = new FileInputStream(source);
             FileOutputStream os = new FileOutputStream(target);
-            InputStream dekryptedStream = cmsDataKryptering.dekrypterData(is);
+            InputStream dekryptedStream = cmsDataKryptering.dekrypterData(is,privatekey)
         ){
             IOUtils.copy(dekryptedStream, os);
         } catch (Exception e) {
